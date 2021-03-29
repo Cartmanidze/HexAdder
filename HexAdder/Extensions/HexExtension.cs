38f3248589bd2a54bb0c221803f1f7cd8236337b
Глеб Безруков <bezrukov96@outlook.com>
2021-03-29 20:47:06 +0300

    Добавьте файлы проекта.

---
+++ HexAdder/Extensions/HexExtension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HexAdder
+{
+    internal static class HexExtension
+    {
+        internal static int HexCharToInt(this char val) => val switch
+        {
+            '0' => 0,
+            '1' => 1,
+            '2' => 2,
+            '3' => 3,
+            '4' => 4,
+            '5' => 5,
+            '6' => 6,
+            '7' => 7,
+            '8' => 8,
+            '9' => 9,
+            'A' or 'a' => 10,
+            'B' or 'b' => 11,
+            'C' or 'c' => 12,
+            'D' or 'd' => 13,
+            'E' or 'e' => 14,
+            'F' or 'f' => 15,
+            _ => throw new ArgumentOutOfRangeException(nameof(val)),
+        };
+
+        internal static char IntToHexChar(this int val) => val switch
+        {
+            0 => '0',
+            1 => '1',
+            2 => '2',
+            3 => '3',
+            4 => '4',
+            5 => '5',
+            6 => '6',
+            7 => '7',
+            8 => '8',
+            9 => '9',
+            10 => 'A',
+            11 => 'B',
+            12 => 'C',
+            13 => 'D',
+            14 => 'E',
+            15 => 'F',
+            _ => throw new ArgumentOutOfRangeException(nameof(val)),
+        };
+    }
+}
---
+++ HexAdder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using HexAdder;
+
+Console.WriteLine("Введите первое шестнадцатеричное число:");
+var a = Console.ReadLine();
+Console.WriteLine("Введите второе шестнадцатеричное число:");
+var b = Console.ReadLine();
+var sum = a.Length > b.Length ? Sum(a, b) : Sum(b, a);
+Console.WriteLine(sum);
+Console.ReadKey();
+
+
+static string Sum(string a, string b)
+{
+    if (a is null or { Length: 0 }) throw new ArgumentException(nameof(a));
+    if (b is null or { Length: 0 }) throw new ArgumentException(nameof(b));
+    int lenght = a.Length;
+    b = b.PadLeft(lenght, '0');
+    int transfer = 0;
+    string result = string.Empty;
+    for (int i = lenght - 1; i >= 0; i--)
+    {
+        int sum = a[i].HexCharToInt() + b[i].HexCharToInt() + transfer;
+        result += (sum % 16).IntToHexChar();
+        transfer = sum / 16;
+    }
+    if (transfer != 0)
+    {
+        result += transfer.IntToHexChar();
+    }
+    return result.Reverse();
+}
